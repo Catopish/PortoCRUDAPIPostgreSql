fc1b30105678d4f391926c12965f8aac319efe72
Catopish <athila2001@gmail.com>
2024-07-16 21:52:36 +0700

    nambah form review movie

--- index.js
+++ index.js
@@ -8,20 +8,42 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-
-app.post("/search", async (req, res) => {
-  const query = req.body.q;
-  let result = await axios.get(
+async function tvApi(query) {
+  const result = await axios.get(
     `https://api.tvmaze.com/search/shows?q=${query}`,
   );
-  result = result.data;
-  // res.send(result);
-  console.log(result);
-  res.render("search", { result });
+  return result;
+}
+
+app.get("/search", async (req, res) => {
+  const query = req.query.q;
+  try {
+    let result = await tvApi(query);
+    result = result.data;
+    res.render("search", { result, query });
+  } catch {
+    // let result = await tvApi(query);
+    // result = result.data;
+    res.send("error");
+  }
+});
+
+app.post("/review", (req, res) => {
+  const movieId = req.body.movieId;
+  const movieTitle = req.body.movieTitle;
+  const movieSummary = req.body.movieSummary;
+  const movieImg = req.body.imgSrc;
+  res.render("newreview", {
+    query: movieId,
+    movieId,
+    movieTitle,
+    movieSummary,
+    movieImg,
+  });
 });
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { query: null });
 });
 
 app.listen(port, () => {
